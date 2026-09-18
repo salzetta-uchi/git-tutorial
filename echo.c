44baa6602542d436d1b94fc3dee486237760aaac
Marco Salzetta <salzetta@uchicago.edu>
2026-09-18 17:48:11 -0500

    echo is 600

--- echo.c
+++ echo.c
@@ -1,6 +1,6 @@
 int main(){
-    char input[500];
-    while(fgets(input, 500, stdin)){  //read from STDIN (aka command-line)
+    char input[600];
+    while(fgets(input, 600, stdin)){  //read from STDIN (aka command-line)
         printf("%s\n", input);  //print out what user typed in
         memset(input, 0, strlen(input));  //reset string to all 0's
     }
